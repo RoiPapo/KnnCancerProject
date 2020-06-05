652173c3033d4e8be78cfd157bd1f956f7a18ff8
Roi <thedarkhermit@gmail.com>
2020-06-05 14:24:43 +0300

    WORKING LAST COMMIT

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main(argv):
     single_final_clusters = single_agro_clustering.run(int(argv[3]))
     Complete_final_clusters = complete_agro_clustering.run(int(argv[3]))
     missions_to_print = argv[2].split(", ")
-
     general_printer(missions_to_print, single_final_clusters, Complete_final_clusters)
 
 
@@ -23,15 +22,15 @@ def general_printer(missions_to_print, single_final_clusters, Complete_final_clu
     :param missions_to_print: single complete or both
     """
     if "Single_Link" in missions_to_print:
+        print("Single Link:")
         for cluster in single_final_clusters:
             print(cluster)
         print("\n")
     if "Complete_Link" in missions_to_print:
-        print("Complete_Link:")
+        print("Complete Link:")
         for cluster in Complete_final_clusters:
             print(cluster)
 
 
 if __name__ == '__main__':
     main(sys.argv)
-
